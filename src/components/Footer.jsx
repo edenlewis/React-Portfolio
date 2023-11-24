c9ed43dfcee88cf6b04285ae9aeaef1c0c940e85
edenlewis <edenlewis80@gmail.com>
2023-11-23 23:55:25 -0700

    header and footer files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <span>Eden Lewis</span>
+            <span>Created 2023</span>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <header>
+            <h1>Portfolio</h1>
+            <nav>
+                <a href="#">About Me</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Resume</a>
+                <a href="#">Contact Me</a>
+            </nav>
+        </header>
+    )
+}
+export default Header
