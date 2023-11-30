6df5bb9f624bf87714998b7a0ae821142ffa9020
edenlewis <edenlewis80@gmail.com>
2023-11-30 13:26:29 -0700

    fixing repo error

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 function Contact() {
   return (
-    <div>
-      <span>My Email: <a href="mailto:example@email.com">example@email.com</a></span>
-      <span>My Phone Number: <a href="tel:+05890000111">(589) 000-0111</a></span>
-      <span>My Github: <a href="https://github.com/edenlewis">https://github.com/edenlewis</a></span>
+    <div className="conflex">
+    <div className="contact">
+      <h4>My Email: <a href="mailto:example@email.com">example@email.com</a></h4>
+      <h4>My Phone Number: <a href="tel:+05890000111">(589) 000-0111</a></h4>
+      <h4>My Github: <a href="https://github.com/edenlewis">https://github.com/edenlewis</a></h4>
+    </div>
     </div>
   );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,15 @@
+import one from '../assets/1.jpg';
+import two from '../assets/2.jpg';
+import three from '../assets/3.jpg';
+import four from '../assets/4.jpg';
+
 function Portfolio(){
     return( 
-        <div>
-        <a href="https://edenlewis.github.io/muscle-munch/"><img src=""></img></a>
-        <img src=""></img>
-        <img src=""></img>
+        <div className="cont">
+        <a href="https://github.com/tannernd/PropPlanner"><img src={one} alt="imgOne" className='img'/></a> 
+        <a href="https://github.com/JBailey007/muscle-munch"><img src={two} alt="imgTwo" className='img'/></a> 
+        <a href="https://github.com/edenlewis/Parent-Connect"><img src={three} alt="imgThree" className='img'/></a> 
+        <a href="https://github.com/tannernd/PropPlanner"><img src={four} alt="imgFour" className='img'/></a> 
         </div>
     )
 }
