7a919f111a3b2e760ad95afc67e8f039f31b2c64
edenlewis <edenlewis80@gmail.com>
2023-12-09 23:57:29 -0700

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 // function App() {
   //   return (
